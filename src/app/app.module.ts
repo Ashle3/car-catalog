1d297d157752fa2c0b522415862703dd01a10f33
Ashle3 <ashleeb3@byui.edu>
2024-04-01 16:58:38 -0500

    cars_list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { CarsListComponent } from './cars-list/cars-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CarsListComponent
   ],
   imports: [
     BrowserModule
